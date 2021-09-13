b4ba5dbefbb8b2cde50695e73ef93608b4914877
jblaauw <j.blaauw@tools4ever.com>
2021-09-13 18:37:11 +0200

    version 2.0.1
    - Minor changes in code
    - Updated readme

--- persons.ps1
+++ persons.ps1
@@ -1,12 +1,12 @@
 #####################################################
 # HelloID-Conn-Prov-Source-PSStuntman-Persons
 #
-# Version: 2.0.0
+# Version: 2.0.1
 #####################################################
 $VerbosePreference = "Continue"
 
 #region functions
-function Get-StuntmanPersons {
+function Get-STPersons {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
@@ -23,50 +23,50 @@ function Get-StuntmanPersons {
         foreach ($user in $stuntman) {
             [System.Collections.Generic.List[object]]$contracts = @()
             $contract = [PSCustomObject]@{
-                ExternalId = "CT$($user.ExternalId)"
-                Title = $user.Title
-                IsManager = $user.IsManager
-                StartDate = $user.StartDate
-                EndDate = $user.EndDate
-                HoursPerWeek = $user.HoursPerWeek
-                Company = $user.Company
-                Department = $user.Department
+                ExternalId           = "CT$($user.ExternalId)"
+                Title                = $user.Title
+                IsManager            = $user.IsManager
+                StartDate            = $user.StartDate
+                EndDate              = $user.EndDate
+                HoursPerWeek         = $user.HoursPerWeek
+                Company              = $user.Company
+                Department           = $user.Department
                 DepartmentExternalId = $user.DepartmentExternalId
-                CostCenter = $user.CostCenter
-                ContractGuid = $user.ContractGuid
+                CostCenter           = $user.CostCenter
+                ContractGuid         = $user.ContractGuid
             }
             $contracts.Add($contract)
 
             $personObj = [PSCustomObject]@{
-                ExternalId = $user.ExternalId
+                ExternalId  = $user.ExternalId
                 DisplayName = $user.DisplayName
-                NickName = $user.GivenName
-                UserId = $user.UserId
-                Status = $user.IsActive
-                UserGuid = $user.UserGuid
+                NickName    = $user.GivenName
+                UserId      = $user.UserId
+                Status      = $user.IsActive
+                UserGuid    = $user.UserGuid
                 Personal = @{
-                    BirthDate = $user.BirthDate
+                    BirthDate  = $user.BirthDate
                     BirthPlace = $user.BirthPlace
-                    Language = $user.Language
+                    Language   = $user.Language
                     Name = @{
-                        GivenName = $user.GivenName
+                        GivenName  = $user.GivenName
                         FamilyName = $user.FamilyName
-                        Initials = $user.Initials
+                        Initials   = $user.Initials
                     }
                     Address = @{
-                        City = $user.City
-                        Street = $user.Street
+                        City        = $user.City
+                        Street      = $user.Street
                         HouseNumber = $user.HouseNumber
-                        ZipCode = $user.ZipCode
+                        ZipCode     = $user.ZipCode
                     }
                     Contact = @{
                         PersonalEmailAddress = $user.PersonalEmailAddress
-                        PersonalPhoneNumber = $user.PersonalPhoneNumber
+                        PersonalPhoneNumber  = $user.PersonalPhoneNumber
                     }
                 }
                 BusinessCommunication = @{
                     BusinessEmailAddress = $user.BusinessEmailAddress
-                    BusinessPhoneNumber = $user.BusinessPhoneNumber
+                    BusinessPhoneNumber  = $user.BusinessPhoneNumber
                 }
                 Contracts = $contracts
             }
@@ -84,4 +84,4 @@ $connectionSettings = ConvertFrom-Json $configuration
 $splatParams = @{
     LocationToPSStuntmanDLL = $connectionSettings.LocationToPSStuntmanDLL
 }
-Get-StuntmanPersons @splatParams
+Get-STPersons @splatParams
